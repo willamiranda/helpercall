e38b8172d3fd616d03ea43ea09e3bd266bb56dde
willamiranda <willmiranda.94@gmail.com>
2024-01-05 17:14:06 -0300

    construindo estrutura de marcação da pagina login

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,34 @@
+import { useState } from 'react'
+import './signin.css'
+
+import logo from '../../assets/logo.png'
+import { Link } from 'react-router-dom'
+
 export default function SignIn() {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
     return (
-        <div>
-            <h1>pagina login</h1>
+        <div className='container-center'>
+            <div className='login'>
+                <div className='login-area'>
+                    <img src={logo} alt='logo do sistema de chamados' />
+                </div>
+                <form>
+                    <h1>Entrar</h1>
+                    <input type='text'
+                        placeholder='exemplo@email.com'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)} />
+                    <input type='password'
+                        placeholder='*****'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)} />
+                    <input type='submit' value="Acessar" />
+                </form>
+                <Link to="/register">Criar uma conta!</Link>
+            </div>
         </div>
     )
 }
