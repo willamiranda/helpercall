f081d7a9d637d48fcb501bbf65bab4df7b8fde66
willamiranda <willmiranda.94@gmail.com>
2024-01-17 16:19:50 -0300

    criando e estilizando component title e pagina de profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,64 @@
-export default function Profile(){
-    return(
+import { useContext, useState } from "react"
+import Header from "../../components/Header"
+import Title from "../../components/Title"
+
+import { FiSettings, FiUpload } from 'react-icons/fi'
+import avatar from '../../assets/avatar.png'
+import { AuthContext } from "../../contexts/auth"
+
+import './profile.css'
+
+export default function Profile() {
+
+    const { user } = useContext(AuthContext)
+    const [avatarUrl, setAvatarUrl] = useState(user && user.avatarUrl)
+
+    return (
         <div>
-            
-        </div>
+            <Header />
+            <div className="content">
+                <Title name="Minha Conta">
+                    <FiSettings size={25} />
+                </Title>
+
+                <div className="container">
+
+                    <form className="form-profile">
+
+                        <label className="label-avatar">
+
+                            <span>
+                                <FiUpload color="#fff" size={25} />
+                            </span>
+
+                            <input type="file" accept="image/*" /> <br />
+
+                            {avatarUrl === null ? (
+                                <img src={avatar} alt="foto de perfil"
+                                    width={250} height={250} />
+                            ) : (<img src={avatarUrl} alt="foto de perfil"
+                                width={250} height={250} />)}
+
+                        </label>
+
+                        <label>Nome</label>
+                        <input type="text" placeholder="Seu nome" />
+
+                        <label>Email</label>
+                        <input type="text" placeholder="teste@teste.com" disabled={true} />
+
+                        <button type="submit"> Salvar</button>
+
+                    </form>
+
+                </div>
+
+                <div className="container">
+                    <button className="logout-btn">Sair</button>
+                </div>
+
+            </div>
+
+        </div >
     )
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
 import Dashboard from "../pages/Dashboard";
+import Profile from "../pages/Profile";
 
 import Private from "./Private";
 
@@ -11,6 +12,7 @@ function RoutesApp() {
             <Route path="/" element={<SignIn />} />
             <Route path="/register" element={<SignUp />} />
             <Route path="/dashboard" element={<Private><Dashboard /></Private>} />
+            <Route path="/profile" element={<Private><Profile /></Private>} />
         </Routes>
     )
 }
