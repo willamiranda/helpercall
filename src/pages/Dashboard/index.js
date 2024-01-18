892cbe84139c1d61a6827c1241276f1ff25d3d18
willamiranda <willmiranda.94@gmail.com>
2024-01-17 22:50:28 -0300

    montando o layout do dashboard e estilizando

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,6 +1,11 @@
 import { useContext } from "react"
 import { AuthContext } from "../../contexts/auth"
 import Header from "../../components/Header";
+import Title from "../../components/Title"
+import { FiPlus, FiMessageSquare, FiSearch, FiEdit2 } from "react-icons/fi"
+
+import { Link } from "react-router-dom";
+import "./dashboard.css"
 
 export default function Dashboard() {
 
@@ -12,9 +17,49 @@ export default function Dashboard() {
 
     return (
         <div>
-            <Header/>
-            <h1>Pagina Dashboard</h1>
-            <button onClick={handleLogout}>Sair da conta</button>
+            <Header />
+            <div className="content">
+                <Title name="Chamados">
+                    <FiMessageSquare />
+                </Title>
+                <>
+                    <Link to="/new" className="new">
+                        <FiPlus color="#FFF" size={25} />
+                        Novo Chamado
+                    </Link>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th scope="col">Cliente</th>
+                                <th scope="col">Assunto</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Cadastrado</th>
+                                <th scope="col">#</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td data-label="Cliente">Mercado Esquina</td>
+                                <td data-label="Assunto">Suporte</td>
+                                <td data-label="Status">
+                                    <span className="badge" style={{ backgroundColor: '#999' }}> Em aberto</span>
+                                </td>
+                                <td data-label="Cadastrado">12/12/2024</td>
+                                <td data-label="#">
+                                    <button className="action" style={{ backgroundColor: '#3583f6' }}>
+                                        <FiSearch color="#FFF" size={17} />
+                                    </button>
+                                    <button className="action" style={{ backgroundColor: '#f6a935' }}>
+                                        <FiEdit2 color="#FFF" size={17} />
+                                    </button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </>
+
+            </div>
+
         </div>
     )
 }
